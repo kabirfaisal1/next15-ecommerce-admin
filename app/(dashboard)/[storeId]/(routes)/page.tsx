0a47d1b57ae6ce741099976f70783b7caa805296
kabirfaisal1 <md.kabirfaisal@outlook.com>
2025-01-03 16:28:05 -0500

    page is fixed

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -3,28 +3,18 @@ import prismadb from '@/lib/prismadb';
 interface DashboardPageProps {
 	params: { storeId: string };
 }
-
 // This is a Server Component by default in the `app` directory
 const DashboardPage = async ({ params }: DashboardPageProps) => {
-	const store = await prismadb.stores.findFirst({
-		where: { id: params.storeId },
+	const store = await prismadb.stores.findUnique({
+		where: {
+			id: params.storeId,
+		},
 	});
-	// const { userId } = await auth();
 
-	// if (!userId) {
-	// 	redirect('/sign-in');
-	// }
-
-	// const stores = await params.stores.findFirst({
-	// 	where: {
-	// 		userId,
-	// 	},
-	// });
 	return (
 		<div>
 			<h1>
-				This is Dashboard Page! StoreName: {store?.name} and storeID:{' '}
-				{store?.id}
+				This is Dashboard Page! StoreName: {store?.name} and storeID: {store?.id}
 			</h1>
 		</div>
 	);
