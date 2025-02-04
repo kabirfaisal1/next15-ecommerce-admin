9d35f342ac1f50b788b4391cedc115caef67fcf6
kabirfaisal1 <md.kabirfaisal@outlook.com>
2025-02-04 11:35:27 -0500

    updating CategoryClient and columns

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -8,22 +8,22 @@ import { useParams, useRouter } from 'next/navigation';
 import Heading from '@/components/ui/heading';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { BillboardColumn, columns } from './columns';
+import { CategoryColumn, columns } from './columns';
 import { DataTable } from '@/components/ui/data-table';
 import { ApiList } from '@/components/ui/api-list';
 
-interface BillboardClientProps {
-	data: BillboardColumn[];
+interface CategoryClientProps {
+	data: CategoryColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
 	const router = useRouter();
 	const params = useParams();
 	return (
 		<React.Fragment>
 			<div className='flex items-center justify-between'>
 				<Heading
-					title={`Billboards (${data.length})`}
+					title={`Categories (${data.length})`}
 					description='Manage billboards for your store'
 				/>
 				<Button
--- app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -3,22 +3,26 @@ import { format } from 'date-fns';
 
 //local imports
 import prismadb from '@/lib/prismadb';
-import { BillboardClient } from './components/client';
-import { BillboardColumn } from './components/columns';
+import { CategoryClient } from './components/client';
+import { CategoryColumn } from './components/columns';
 
-const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
-	const billboards = await prismadb.billboards.findMany({
+const CategoriesPage = async ({ params }: { params: { storeId: string } }) => {
+	const categories = await prismadb.categories.findMany({
 		where: {
 			storeId: params.storeId,
+		},
+		include: {
+			billboard: true,
 		},
 		orderBy: {
 			createdAt: 'desc',
 		},
 	});
 
-	const formattedBillboards: BillboardColumn[] = billboards.map(item => ({
+	const formattedCategories: CategoryColumn[] = categories.map(item => ({
 		id: item.id,
-		label: item.label,
+		name: item.name,
+		billboardLabel: item.billboard.label,
 		createdAt: format(item.createdAt, 'MMMM do, yyyy'),
 		updatedAt: format(item.updatedAt, 'MMMM do, yyyy'),
 	}));
@@ -26,10 +30,11 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
 	return (
 		<div className='flex-col'>
 			<div className='flex-1 space-y-4 p-8 pt-6'>
-				<BillboardClient data={formattedBillboards} />
+				Main //TODO Delete
+				<CategoryClient data={formattedCategories} />
 			</div>
 		</div>
 	);
 };
 
-export default BillboardsPage;
+export default CategoriesPage;
