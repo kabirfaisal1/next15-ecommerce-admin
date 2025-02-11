5fc168d9e1eb2d0bffaeb9a5b872c33cc7796878
kabirfaisal1 <md.kabirfaisal@outlook.com>
2025-02-10 21:24:39 -0500

    fixing test

--- test/cypress/e2e/UI/regression/uiCategories.cy.ts
+++ test/cypress/e2e/UI/regression/uiCategories.cy.ts
@@ -3,8 +3,8 @@ import AdminCategoriesPage from '@support/test_components/pages/uiAdminCategorie
 
 
 
-const storeId = 'c936295f-6653-48f3-b0f4-be89bcc331b8';
-const billboardId = 'a3e3e2ea-5707-45bb-a689-86dfb9d917d6';
+const storeId = '00ea910b-ecf0-4d1e-bcb6-5cb72313bb46';
+const billboardId = '8cf24cca-eade-4952-961a-58d760b73fef';
 const adminCategoriesPage = new AdminCategoriesPage();
 describe( 'Store Billboard', () =>
 {
@@ -19,40 +19,41 @@ describe( 'Store Billboard', () =>
     {
         it( 'User navigate to Categories', () =>
         {
-            cy.log( 'Going to billboards from navigation tab' );
+            cy.log( 'Going to categories from navigation tab' );
             cy.navigateTabItem( "Categories" );
 
         } );
         it( 'User create new Categories', () =>
         {
-            cy.step( 'Going to billboards from navigation tab' );
+            cy.step( 'Going to categories from navigation tab' );
             cy.visit( `/${storeId}/categories` );
             adminCategoriesPage.clickOnAddCategoryButton( storeId );
             adminCategoriesPage.enterCategoryName( 'cyCreateCategories' );
-            adminCategoriesPage.selectBillboard( 'Forever Billboard' );
+            adminCategoriesPage.selectBillboard( 'Do not delete' );
             adminCategoriesPage.clickOnSubmitButton( storeId, billboardId );
 
 
         } );
-        it( 'User update Categories', () =>
+        //TODO check why this failed
+        it.only( 'User update Categories', () =>
         {
-            cy.step( 'Going to billboards from navigation tab' );
+            cy.step( 'Going to categories from navigation tab' );
             cy.visit( `/${storeId}/categories` );
             const categoryId = Cypress.env( 'categoryId' );
             adminCategoriesPage.actionModifyCategory( 'cyCreateCategories' );
             adminCategoriesPage.enterCategoryName( 'cyUpdatedCategoriess' );
-            adminCategoriesPage.selectBillboard( 'Forever Billboard' );
+            adminCategoriesPage.selectBillboard( 'Do not delete' );
             adminCategoriesPage.clickOnSubmitButton( storeId, billboardId, categoryId );
         } );
         it( 'User Delete Categories', () =>
         {
-            cy.step( 'Going to billboards from navigation tab' );
+            cy.step( 'Going to categories from navigation tab' );
             cy.visit( `/${storeId}/categories` );
             adminCategoriesPage.actionDeleteCategory( 'cyUpdatedCategoriess' );
         } );
-        it( 'User create Categories without billboards', () =>
+        it( 'User create Categories without categories', () =>
         {
-            cy.step( 'Going to billboards from navigation tab' );
+            cy.step( 'Going to categories from navigation tab' );
             cy.visit( `/${storeId}/categories/new` );
 
             adminCategoriesPage.enterCategoryName( 'noBillBillboard' );
@@ -61,19 +62,19 @@ describe( 'Store Billboard', () =>
         } );
         it( 'User create Categories without Name', () =>
         {
-            cy.step( 'Going to billboards from navigation tab' );
+            cy.step( 'Going to categories from navigation tab' );
             cy.visit( `/${storeId}/categories/new` );
 
-            adminCategoriesPage.selectBillboard( 'Forever Billboard' );
+            adminCategoriesPage.selectBillboard( 'Do not delete' );
             adminCategoriesPage.formErrorValidation( 'Name must be at least 3 characters long' );
 
         } );
         it( 'User create Categories with 2 characters Name', () =>
         {
-            cy.step( 'Going to billboards from navigation tab' );
+            cy.step( 'Going to categories from navigation tab' );
             cy.visit( `/${storeId}/categories/new` );
             adminCategoriesPage.enterCategoryName( 'cy' );
-            adminCategoriesPage.selectBillboard( 'Forever Billboard' );
+            adminCategoriesPage.selectBillboard( 'Do not delete' );
             adminCategoriesPage.formErrorValidation( 'Name must be at least 3 characters long' );
 
         } );
